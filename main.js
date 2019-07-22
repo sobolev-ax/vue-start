a68b033e2fa00c8003bfcf86aad68d994093ecf0
Alexander Sobolev <sobolev.ax@gmail.com>
2019-07-22 23:34:18 +0300

    added counter

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
 new Vue({
   el: '#app',
   data: {
+    counter: 0,
+    increased: '',
     name: 'Sasha',
     google: 'https://google.com',
     link: '<a href="https://ya.ru">go to yandex....</a>'
   },
   methods: {
+    increaseCounter: function(event, num) {
+      this.counter = this.counter + num;
+      this.increased = ' +' + num;
+
+      event.target.classList.add('clicked');
+      setTimeout(function() {
+        event.target.classList.remove('clicked');
+      }, 500)
+    },
     sayHello: function() {
       alert('Hello ' + this.name + '!');
     },
