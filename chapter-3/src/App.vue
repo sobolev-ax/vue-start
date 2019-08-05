69e62e527713bfc6d1939edc94ac689d4f617430
Alexander Sobolev <sobolev.ax@gmail.com>
2019-08-05 22:09:33 +0300

    Added Car.vue component

--- chapter-3/src/App.vue
+++ chapter-3/src/App.vue
@@ -1,9 +1,16 @@
 <template>
-  <h1>{{ msg }}</h1>
+  <div>
+    <h1>{{ msg }}</h1>
+    <app-car></app-car>
+  </div>
 </template>
 
 <script>
+import Car from './Car'
 export default {
+  components: {
+    appCar: Car,
+  },
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
